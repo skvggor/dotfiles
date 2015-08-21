003f244c6718f7af1e6f13c94f7353a33416952a
Marcos Garcia de Lima <marckfree@gmail.com>
2015-08-21 17:41:17 -0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,33 +1,23 @@
-# Exports
-set -x GOPATH /home/xxx/Dropbox/Programming/Go $GOPATH
-set -xg PATH /usr/local/go/bin $PATH
-set -xg PATH /usr/local/share/scala/bin $PATH
-set -xg PATH /home/xxx/Qt/5.3/gcc_64/bin $PATH
-set -xg PATH /home/xxx/Elm-Platform/0.13/.cabal-sandbox/bin $PATH
-set -xg CHROME_DEVEL_SANDBOX /usr/local/sbin/chrome-devel-sandbox $CHROME_DEVEL_SANDBOX
-
-# Alias
-alias sudo="sudo "
-alias cabal="/home/xxx/.cabal/bin/cabal"
-alias nw="/home/xxx/Copy/Apps/node-webkit-v0.10.2-linux-x64/nw"
-
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme coffeeandcode
-
-# Base 16 themes
-#eval sh $HOME/.config/base16-shell/base16-chalk.sh
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-
-# Path to your custom folder (default path is $FISH/custom)
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+
 set fish_greeting ""
+
+# Alias
+if test (uname) = Darwin
+	alias sassc="~/Projects/sassc/bin/./sassc"
+	alias subl="open -a /Applications/Sublime\ Text.app/Contents/MacOS/Sublime\ Text"
+	alias chm="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk"
+end
+
+Theme 'l'
+Plugin 'theme'
