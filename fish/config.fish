39b63520d00f995caa3122f1c925636259cc78e6
Marcos Garcia de Lima <marckfree@gmail.com>
2015-09-24 18:04:51 -0300

    Add alias for grunt

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,8 @@ set -g OMF_CONFIG $HOME/.config/omf
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
+set fish_greeting ""
+
 # Aliases
 if test (uname) = Darwin
         function subl
@@ -21,4 +23,8 @@ if test (uname) = Darwin
         function chm
                 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $argv;
         end
+end
+
+function g
+	grunt $argv;
 end
