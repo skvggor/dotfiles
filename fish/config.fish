af839f95a00f067b5eb02c5a053726d18d5b2607
Marcos Garcia de Lima <marckfree@gmail.com>
2017-10-17 15:26:59 -0200

    Add iphone alias

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set me (whoami)
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 
-source $fisher_home/config.fish
+source $fisher_home/fisher.fish
 
 set fish_greeting ""
 
@@ -17,6 +17,10 @@ if test (uname) = Darwin
                 open -a /Applications/Sublime\ Text.app/Contents/MacOS/Sublime\ Text
         end
 
+	function brackets
+		open -a /Applications/Brackets.app/Contents/MacOS/Brackets $argv;
+	end
+
         function chm
                 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $argv;
         end
@@ -27,6 +31,10 @@ if test (uname) = Darwin
 
 	function frio
 		python /Users/$me/Projects/Labs/snow.py
+	end
+
+	function iphone
+		open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
 	end
 
 	# Go Lang
@@ -41,3 +49,6 @@ function g
         grunt $argv;
 end
 
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/lib/ruby/gems/2.2.0/bin/bundler" $fish_user_paths
+set -g fish_user_paths "/usr/local/lib/ruby/gems/2.2.0/bin/bundle" $fish_user_paths
