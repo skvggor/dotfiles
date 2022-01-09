ce0d6310345b4d56b88c5d2f1a594879805bf664
Marcos Garcia de Lima <marckfree@gmail.com>
2022-01-09 20:03:39 -0300

    Change docker order

--- first-install.sh
+++ first-install.sh
@@ -5,6 +5,13 @@ mkdir ~/Google\ Drive;
 # sudo add-apt-repository ppa:agornostal/ulauncher;
 # sudo add-apt-repository ppa:gezakovacs/ppa;
 
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+sudo chmod 666 /var/run/docker.sock
+# ---
+
 # apt packages
 sudo apt update && sudo apt install -y \
 alacritty \
@@ -53,13 +60,6 @@ xclip;
 # xscreensaver \
 # xscreensaver-data-extra \
 # xscreensaver-gl-extra;
-
-# docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-sudo chmod 666 /var/run/docker.sock
-# ---
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
