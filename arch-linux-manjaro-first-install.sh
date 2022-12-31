f5d5eeaacbb13ea43699a8c87924f09692d5a648
Marcos Garcia de Lima <marckfree@gmail.com>
2022-12-31 01:06:42 -0300

    Update packages (arch)

--- arch-linux-manjaro-first-install.sh
+++ arch-linux-manjaro-first-install.sh
@@ -4,7 +4,7 @@ mkdir -v ~/.config/{pulse,lsd,fish,darktable};
 mkdir ~/Google\ Drive;
 mkdir -p ~/Projects/me;
 
-sudo pacman -Syu
+sudo pacman -Syu;
 
 # check alternatives
 # =====
@@ -16,29 +16,85 @@ sudo pacman -Syu
 # ttf-mscorefonts-installer
 
 sudo pacman -S curl \
-cheese
-cmatrix
-darktable
-filezilla
-fish
-flameshot
-git
-golang
-gpick
-inkscape
-jq
-kdenlive
-mplayer
-net-tools
-optipng
-python-pip
-rhythmbox
-simplescreenrecorder
-solaar
-tmux
-vim
-vlc
-xclip
+cheese \
+cmake \
+cmatrix \
+darktable \
+filezilla \
+fish \
+flameshot \
+git \
+golang \
+gpick \
+htop \
+inkscape \
+jq \
+kdenlive \
+lsd \
+mplayer \
+net-tools \
+nodejs \
+npm \
+optipng \
+python-pip \
+rhythmbox \
+simplescreenrecorder \
+solaar \
+tmux \
+vim \
+vlc \
+xclip \
+yarn;
 
-sudo yay -S ulauncher \
+yay -S ulauncher \
+espanso \
+figma-linux \
+google-chrome \
+heroku \
+starship;
 
+cp -rv fish/config.fish ~/.config/fish/;
+cp -rv .gitconfig ~/;
+cp -rv alacritty ~/.config/;
+cp -rv darktable/styles ~/.config/darktable/styles/;
+cp -rv pulse.conf ~/.config/pulse/daemon.conf;
+cp -rv SimpleScreenRecorder/.ssr ~/;
+cp -rv lsd/config.yaml ~/.config/lsd/;
+
+# tmux conf
+cp -rv .tmux.conf ~/;
+tmux source ~/.tmux.conf;
+
+# change shell
+chsh -s /usr/bin/fish;
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
+
+# npm global packages
+sudo npm i -g \
+begynner \
+easy-rename \
+gtop \
+localtunnel \
+n \
+npm-check-updates \
+qrcode-terminal \
+svgo \
+vercel;
+
+# pfetch
+{
+  mkdir ~/temp && cd ~/temp && \
+  git clone https://github.com/dylanaraps/pfetch && \
+  cd pfetch && \
+  sudo cp -v pfetch /usr/bin && \
+  cd ~ && rm -rf ~/temp;
+}
+
+# nerd fonts
+{
+  mkdir ~/temp && cd ~/temp && \
+  git clone https://github.com/ryanoasis/nerd-fonts && \
+  bash nerd-fonts/install.sh && \
+  cd ~ && rm -rf ~/temp;
+}
