daad10eb28063d2fe2dc189cad0ee92fa75d086d
Marcos Garcia de Lima <marckfree@gmail.com>
2024-02-25 15:55:48 -0300

    Update with:
    - Remove github copilot assistant
    - Add zoxide pack

--- fish/config.fish
+++ fish/config.fish
@@ -1,77 +1,81 @@
+set PATH $HOME/.cargo/bin $PATH
+
 set fish_greeting ""
 
 function camera
-	mplayer tv:// -tv driver=v4l2:width=1024:height=768 -vo xv
+    mplayer tv:// -tv driver=v4l2:width=1024:height=768 -vo xv
 end
 
 function ns
-	npm start
+    npm start
 end
 
 function ydev
-	yarn dev
+    yarn dev
 end
 
 function ygen
-	yarn generate
+    yarn generate
 end
 
 function dev
-	npm run dev
+    npm run dev
 end
 
 function postman
-	/bin/Postman/./Postman
+    /bin/Postman/./Postman
 end
 
 function android-studio
-	/opt/android-studio/bin/./studio.sh
+    /opt/android-studio/bin/./studio.sh
 end
 
 function sshadd
-	eval (ssh-agent -c)
+    eval (ssh-agent -c)
 end
 
 function code
-	/usr/bin/./code-insiders $argv
+    /usr/bin/./code-insiders $argv
 end
 
 function ls
-	lsd $argv
+    lsd $argv
 end
 
 function cat
-	bat --theme=Dracula $argv
+    bat --theme=Dracula $argv
 end
 
 function catn
-	cat --style="changes" $argv
+    cat --style="changes" $argv
 end
 
 function firefox-nightly
-	/usr/bin/firefox-nightly/./firefox
+    /usr/bin/firefox-nightly/./firefox
 end
 
 function scr
-	cmatrix -s -b
+    cmatrix -s -b
 end
 
-function how
-	github-copilot-cli what-the-shell $argv
-end
+# function how
+#     github-copilot-cli what-the-shell $argv
+# end
 
-function ga
-	github-copilot-cli git-assist $argv
-end
+# function ga
+#     github-copilot-cli git-assist $argv
+# end
 
-function gha
-	github-copilot-cli gh-assist $argv
-end
+# function gha
+#     github-copilot-cli gh-assist $argv
+# end
 
-abbr -a \?\? github-copilot-cli what-the-shell
-abbr -a git\? github-copilot-cli git-assist
-abbr -a gh\? github-copilot-cli gh-assist
+# abbr -a \?\? github-copilot-cli what-the-shell
+# abbr -a git\? github-copilot-cli git-assist
+# abbr -a gh\? github-copilot-cli gh-assist
 
 atuin init fish | source
 starship init fish | source
+zoxide init fish | source
+
 nitch; and scr
