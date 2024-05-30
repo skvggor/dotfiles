f782d27e85330a726e545fd922c6ff543d18fc80
Marcos Garcia de Lima <marckfree@gmail.com>
2024-05-30 15:07:50 -0300

    Add starship config + update fish shell config

--- fish/config.fish
+++ fish/config.fish
@@ -6,22 +6,6 @@ function camera
     mplayer tv:// -tv driver=v4l2:width=1024:height=768 -vo xv
 end
 
-function ns
-    npm start
-end
-
-function ydev
-    yarn dev
-end
-
-function ygen
-    yarn generate
-end
-
-function dev
-    npm run dev
-end
-
 function postman
     /bin/Postman/./Postman
 end
@@ -50,32 +34,110 @@ function catn
     cat --style="changes" $argv
 end
 
-function firefox-nightly
-    /usr/bin/firefox-nightly/./firefox
-end
-
 function scr
     cmatrix -s -b
 end
 
-# function how
-#     github-copilot-cli what-the-shell $argv
-# end
+# =============================================================================
+#
+# Utility functions for zoxide.
+#
 
-# function ga
-#     github-copilot-cli git-assist $argv
-# end
+# pwd based on the value of _ZO_RESOLVE_SYMLINKS.
+function __zoxide_pwd
+    builtin pwd -L
+end
 
-# function gha
-#     github-copilot-cli gh-assist $argv
-# end
+# A copy of fish's internal cd function. This makes it possible to use
+# `alias cd=z` without causing an infinite loop.
+if ! builtin functions --query __zoxide_cd_internal
+    if builtin functions --query cd
+        builtin functions --copy cd __zoxide_cd_internal
+    else
+        alias __zoxide_cd_internal='builtin cd'
+    end
+end
 
-# abbr -a \?\? github-copilot-cli what-the-shell
-# abbr -a git\? github-copilot-cli git-assist
-# abbr -a gh\? github-copilot-cli gh-assist
+# cd + custom logic based on the value of _ZO_ECHO.
+function __zoxide_cd
+    __zoxide_cd_internal $argv
+end
+
+# =============================================================================
+#
+# Hook configuration for zoxide.
+#
+
+# Initialize hook to add new entries to the database.
+function __zoxide_hook --on-variable PWD
+    test -z "$fish_private_mode"
+    and command zoxide add -- (__zoxide_pwd)
+end
+
+# =============================================================================
+#
+# When using zoxide with --no-cmd, alias these internal functions as desired.
+#
+
+if test -z $__zoxide_z_prefix
+    set __zoxide_z_prefix 'z!'
+end
+set __zoxide_z_prefix_regex ^(string escape --style=regex $__zoxide_z_prefix)
+
+# Jump to a directory using only keywords.
+function __zoxide_z
+    set -l argc (count $argv)
+    if test $argc -eq 0
+        __zoxide_cd $HOME
+    else if test "$argv" = -
+        __zoxide_cd -
+    else if test $argc -eq 1 -a -d $argv[1]
+        __zoxide_cd $argv[1]
+    else if set -l result (string replace --regex $__zoxide_z_prefix_regex '' $argv[-1]); and test -n $result
+        __zoxide_cd $result
+    else
+        set -l result (command zoxide query --exclude (__zoxide_pwd) -- $argv)
+        and __zoxide_cd $result
+    end
+end
+
+# Completions.
+function __zoxide_z_complete
+    set -l tokens (commandline --current-process --tokenize)
+    set -l curr_tokens (commandline --cut-at-cursor --current-process --tokenize)
+
+    if test (count $tokens) -le 2 -a (count $curr_tokens) -eq 1
+        # If there are < 2 arguments, use `cd` completions.
+        complete --do-complete "'' "(commandline --cut-at-cursor --current-token) | string match --regex '.*/$'
+    else if test (count $tokens) -eq (count $curr_tokens); and ! string match --quiet --regex $__zoxide_z_prefix_regex. $tokens[-1]
+        # If the last argument is empty and the one before doesn't start with
+        # $__zoxide_z_prefix, use interactive selection.
+        set -l query $tokens[2..-1]
+        set -l result (zoxide query --exclude (__zoxide_pwd) --interactive -- $query)
+        and echo $__zoxide_z_prefix$result
+        commandline --function repaint
+    end
+end
+complete --command __zoxide_z --no-files --arguments '(__zoxide_z_complete)'
+
+# Jump to a directory using interactive search.
+function __zoxide_zi
+    set -l result (command zoxide query --interactive -- $argv)
+    and __zoxide_cd $result
+end
+
+# =============================================================================
+#
+# Commands for zoxide. Disable these using --no-cmd.
+#
+
+abbr --erase cd &>/dev/null
+alias cd=__zoxide_z
+
+abbr --erase cdi &>/dev/null
+alias cdi=__zoxide_zi
 
 atuin init fish | source
 starship init fish | source
 zoxide init fish | source
-
 nitch; and scr
